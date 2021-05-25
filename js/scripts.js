6bd10f70a7855738e9bcc625a9842dd942b5eca5
flowlian <juliand182@gmail.com>
2021-05-25 23:23:49 +0200

    create script for the menuToggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log ("Hola! I am Julián. Welcome to My Portfolio Site");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
