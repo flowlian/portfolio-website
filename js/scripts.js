83ff5e87a68344d681f0e5b68a33dd426a989e4e
flowlian <juliand182@gmail.com>
2021-05-27 13:29:48 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,14 @@
 console.log ("Hola! I am Julián. Welcome to My Portfolio Site");
 
 
+
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle' && screen.width < 640) {
+x.className += ' responsive';
+} else {
+x.className = 'navtoggle';
+}
 }
 
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+//
